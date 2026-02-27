a9b53a3d611da893e790b97258d506d019e870d8
neilcampbelll <neil.campbelll@gmail.com>
2026-02-27 13:13:11 +0000

    updated file name handling and time/date formats which vary by year

--- data_create_tacsat.R
+++ data_create_tacsat.R
@@ -1,19 +1,18 @@
 for (years in (2011:2025)){
 
-  ## note - the naming protocol changes in 2022, so this is a bit of a fix - not ideal
+  ## note - the file naming protocol changes in 2022 - this is a bit of a fix - not ideal
   vms_file <- list.files(
     path = paste0("data/", years, "/VMS/"),
     pattern = paste0("^(VMS[_-]", years, "|", years, "-VMS)\\.csv$"),
     full.names = TRUE
   )
   
-  # Read the file
+  # reads in the file
   no.vms <- read.table(vms_file, header = T, sep = ";", dec = ",")
 
 no.vms <- as.data.frame(no.vms)
 
 tacsat2 <- no.vms %>%
-  # Select and rename columns
   select(
     VE_REF = Radiokallesignal,
     SI_LATI = Breddegrad,
@@ -22,17 +21,20 @@ tacsat2 <- no.vms %>%
     SI_SP = Fart,
     SI_HE = Kurs
   ) %>%
-  # Convert datetime and split into date and time
   mutate(
-    datetime = {
-      dt <- as.POSIXct(datetime, format = "%d.%m.%Y  %H:%M:%S")
-      if (all(is.na(dt))) dt <- as.POSIXct(datetime, format = "%d-%b-%y %H.%M.%OS")
-      dt
-    },
+    #  remove trailing zeroes
+    datetime_clean = sub("[,.].0{5,}.*$", "", datetime), 
+    
+    # handling the multiple date formats (check each year!)
+    datetime = parse_date_time(datetime_clean, orders = c(
+      "d.m.Y H:M:S",      # Original (2011 - 2021)
+      "d.m.Y H.M.S",      # 2022 & 2023 format
+      "d-b-y H.M.S"       # 2024 format (e.g., 01-JAN-24)
+    )),
+    
     SI_DATE = format(datetime, "%d/%m/%Y"),
     SI_TIME = format(datetime, "%H:%M")
-  )%>%
-  # Select final columns in the correct order
+  ) %>%
   select(VE_REF, SI_LATI, SI_LONG, SI_DATE, SI_TIME, SI_SP, SI_HE)
 
 print(years)
